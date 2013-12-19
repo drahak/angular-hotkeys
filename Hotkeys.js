cbf3906c7bc0adf7684df3383b58c9dbdb54a2c1
Drahomír Hanák <drahomir.hanak@gmail.com>
2013-12-19 15:50:22 +0100

    feat(hotkeys): added directive

--- Hotkeys.js
+++ Hotkeys.js
@@ -4,6 +4,26 @@
 
 	var hotKeys = angular.module('drahak.hotkeys', []);
 
+	hotKeys.directive('hotkey', ['$parse', 'ParseKey', function($parse, ParseKey) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+				var invoker = $parse(attrs.hotkeyAction);
+				var hotKey = ParseKey(attrs.hotkey);
+				var keys = [];
+				element.bind('keydown', function(e) {
+					keys.push(e.keyCode);
+					if (keys.sort().join(',') === hotKey.sort().join(',')) {
+						invoker(scope, { $event: e });
+					}
+				});
+				element.bind('keyup', function(e) {
+					keys.splice(keys.indexOf(e.keyCode), 1);
+				})
+			}
+		}
+	}]);
+
 	hotKeys.service('ParseKey', function() {
 		var lexer = {
 			'backspace': 8,
